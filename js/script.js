f52eb88d2f4c73a02e4223e79f06fa32c2abf488
Leonid <getgotgotgot@gmail.com>
2022-02-04 00:55:17 +0300

    Added Axios

--- js/script.js
+++ js/script.js
@@ -182,9 +182,16 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
             });
         });
