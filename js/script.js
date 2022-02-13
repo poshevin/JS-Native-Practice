6db69c85f3a0ce648b839f21c3145a652ccdefb0
Leonid <getgotgotgot@gmail.com>
2022-02-13 03:27:48 +0300

    feat: Added function with regular expressions

--- js/script.js
+++ js/script.js
@@ -300,6 +300,11 @@ document.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 1,
         offset = 0;
 
+    function onlyDigitals(e) {
+        let number = +e.replace(/\D/g, '');
+        return number;
+    }
+
     function currentText() {
         if (slides.length < 10) {
             currentNumber.textContent = `0${slideIndex}`;
@@ -376,10 +381,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     arrowNext.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == onlyDigitals(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset +=  +width.slice(0, width.length - 2);
+            offset +=  onlyDigitals(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -397,9 +402,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     arrowPrev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = onlyDigitals(width) * (slides.length - 1);
         } else {
-            offset -=  +width.slice(0, width.length - 2);
+            offset -=  onlyDigitals(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -420,12 +425,11 @@ document.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = +width.replace(/\D/g, '') * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            dots.forEach(dot => dot.style.opacity = '.5');
-            dots[slideIndex - 1].style.opacity = 1;
+            visibleDots();
 
             currentText();
         });
